4d5a0b611ea1023fe0833b27b5c83a92997400d2
Torben W Laursen <twl@fakturaservice.dk>
2022-01-14 11:44:06 +0100

    moved all project files one scope up to match composers requirments

--- example/MyAccountingProgram.php
+++ example/MyAccountingProgram.php
@@ -15,7 +15,7 @@
 
 /** Composer Autoload to define Namespaces */
 if ( file_exists(dirname(__FILE__).'/../vendor/autoload.php') ) {
-    require_once dirname(__FILE__).'/../vendor/autoload.php';
+    require_once dirname(__FILE__) . '/../vendor/autoload.php';
 }
 
 
