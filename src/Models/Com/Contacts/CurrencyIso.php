45bdf2f388e00898803a7821a43f21118e11a450
Torben W Laursen <twl@fakturaservice.dk>
2022-08-02 14:07:18 +0200

    Updated orsdk currencies enums on Contacts model (Uppercase)

--- src/Models/Com/Contacts/CurrencyIso.php
+++ src/Models/Com/Contacts/CurrencyIso.php
@@ -4,13 +4,15 @@ namespace OrSdk\Models\Com\Contacts;
 
 use OrSdk\Util\BasicEnum;
 
-abstract class CurrencyIso extends \BasicEnum
+abstract class CurrencyIso extends BasicEnum
 {
-    const dkk	= 'dkk';
-    const sek	= 'sek';
-    const nok	= 'nok';
-    const usd	= 'usd';
-    const eur	= 'eur';
-    const cny	= 'cny';
-    const gbp	= 'gbp';
+    const DKK	= 'DKK';
+    const SEK	= 'SEK';
+    const NOK	= 'NOK';
+    const USD	= 'USD';
+    const EUR	= 'EUR';
+    const CNY	= 'CNY';
+    const GBP	= 'GBP';
 }
+
+
