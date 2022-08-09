96a2a7ca6eaa938472dd62dea60530f60922fae1
Torben W Laursen <twl@fakturaservice.dk>
2022-08-09 17:04:30 +0200

    Updated orsdk CurrencyIso enums on Entries model (Uppercase)

--- src/Models/Com/Entries/CurrencyIso.php
+++ src/Models/Com/Entries/CurrencyIso.php
@@ -5,8 +5,13 @@ namespace OrSdk\Models\Com\Entries;
 
 use OrSdk\Util\BasicEnum;
 
-abstract class CurrencyIso extends BasicEnum
+abstract class CurrencyIso extends \BasicEnum
 {
-    const dkk	= 'dkk';
-    const eur	= 'eur';
+    const DKK	= 'DKK';
+    const SEK	= 'SEK';
+    const NOK	= 'NOK';
+    const USD	= 'USD';
+    const EUR	= 'EUR';
+    const CNY	= 'CNY';
+    const GBP	= 'GBP';
 }
