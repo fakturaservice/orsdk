6abed201babc322f630bc110ef623ebaecb130f4
fakturaserviceTWL <twl@fakturaservice.dk>
2023-12-18 11:12:53 +0100

    Added DispatchStatus Enums for Document model

---
+++ src/Models/Com/Documents/DispatchStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace OrSdk\Models\Com\Documents;
+
+use OrSdk\Util\BasicEnum;
+
+abstract class DispatchStatus extends BasicEnum
+{
+    const notToBeSent	= 'notToBeSent';
+    const pending    	= 'pending';
+    const failed     	= 'failed';
+    const sent       	= 'sent';
+    const received   	= 'received';
+}
