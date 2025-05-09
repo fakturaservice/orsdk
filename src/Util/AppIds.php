e642cc62223705ef1ccc553725028aca5713112b
fakturaserviceTWL <twl@fakturaservice.dk>
2025-05-09 13:53:23 +0200

    Adding all new APPs ids

--- src/Util/AppIds.php
+++ src/Util/AppIds.php
@@ -12,9 +12,19 @@ namespace OrSdk\Util;
  */
 abstract class AppIds extends BasicEnum
 {
-    const EAN           = 1;
+    const EAN           = 1;//Deprecated
     const Reminder      = 2;
     const InvoiceLayout = 3;
     const BankConnect   = 4;
     const SMTP          = 5;
+    const Danlon        = 6;
+    const Datalon       = 7;
+    const Salary        = 8;
+    const MobilePay     = 9;
+    const International = 10;
+    const Prolon        = 11;
+    const ReceiveEAN    = 12;//Deprecated
+    const VATReporting  = 13;
+    const eDelivery     = 14;
+    const MobilePayQR   = 15;
 }
